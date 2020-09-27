6cde26a53a4a3d23f80ced83ab847f573f3a6974
hanodims <hend--mohammed@outlook.com>
2020-09-27 23:32:59 +0300

    story.js

---
+++ story.js
@@ -0,0 +1,3 @@
+let name = "Hend", age = 25, hobby = "Reading";
+
+console.log("Mr./Mrs.", name,", in",age, "years, you will be ", age + 33 ," years old. By then you'll have decades of ",hobby," practice!")
