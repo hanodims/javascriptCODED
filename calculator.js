9ae03487a10f1ff37c3548ee8187a17662cd629c
hanodims <hend--mohammed@outlook.com>
2020-09-27 23:46:04 +0300

    calculator.js

---
+++ calculator.js
@@ -0,0 +1,10 @@
+let firtst =5 , second = 4, oper = "-";
+
+if (oper == "+"){
+    console.log(firtst+second)
+} else if (oper == "-"){
+    console.log(firtst-second)
+} else if (oper == "*"){
+    console.log(firtst*second)
+} else if (oper == "/")
+    console.log(firtst/second)
